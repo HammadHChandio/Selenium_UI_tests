1df14746424211e6a1090f90e454ae6cad2703e6
Hammad Chandio <hammad.hyder.chandio@outlook.com>
2017-03-31 19:43:38 +0100

    Added Automation Project

---
+++ Selenium.UI.test/Drivers/Driverhook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace Selenium.UI.test.Drivers
+{
+    [Binding]
+    public sealed class Driverhook
+    {
+        // public static IWebDriver driver = new RemoteWebDriver(new Uri("http://192.168.0.49:4444/wd/hub"), DesiredCapabilities.Firefox());
+
+
+        //public static IWebDriver driver = new RemoteWebDriver(new Uri("http://192.168.0.49:4444/wd/hub"));
+
+       public static IWebDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            DesiredCapabilities cap = DesiredCapabilities.Chrome();
+            cap.SetCapability("browserName", "chrome");
+            cap.SetCapability("seleniumProtocol", "WebDriver");
+            cap.SetCapability("version", "ANY");
+            cap.SetCapability("maxInstances", "1");
+            cap.SetCapability("maxsession", "3");
+
+        driver = new RemoteWebDriver(new Uri("http://192.168.0.21:4444/wd/hub"), DesiredCapabilities.Chrome());
+    }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Selenium.UI.test/Features/SearchBar.feature
@@ -0,0 +1,14 @@
+﻿Feature: SearchBar
+	In order to search for Multiple items on amazon 
+	As a customer 
+	I want to be able to search for specific item 
+
+Background: 
+Given Amazon site is available
+
+@Search 
+Scenario: Searching for Items
+	Given Search bar is available 
+	And I want to search for "Thinkpad" 
+	When I press search 
+	Then I should see result for thinkpads
