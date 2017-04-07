da51bd457a3ac06bbc37c31fcaf20d8b8ff91cac
Hammad Chandio <hammad.hyder.chandio@outlook.com>
2017-04-07 19:48:43 +0100

    Intialising the project

---
+++ Selenium.UI.test/Steps/SearchBarSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using Selenium.UI.test.Drivers;
+
+namespace Selenium.UI.test.Steps
+{
+    [Binding]
+    public class SearchBarSteps
+    {
+       
+
+        [Given(@"Amazon site is available")]
+        public void GivenAmazonSiteIsAvailable()
+        {
+            Driverhook.driver.Navigate().GoToUrl("https://www.amazon.co.uk");
+        }
+        
+        [Given(@"Search bar is available")]
+        public void GivenSearchBarIsAvailable()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I want to search for ""(.*)""")]
+        public void GivenIWantToSearchFor(string item)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press search")]
+        public void WhenIPressSearch()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see result for thinkpads")]
+        public void ThenIShouldSeeResultForThinkpads()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
